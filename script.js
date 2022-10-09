aee5e5d9be474d62270e5a5f4938f1e328602216
Garvit Galgat <59741135+gg-dev-05@users.noreply.github.com>
2022-10-09 18:16:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
-// const timeouts = makeArray(20, 20);
+const FADEOUT_TIME = 1000;
 window.onload = () => {
+  let touch = false;
+  if ("ontouchstart" in document.documentElement) {
+    console.log("USING TOUCH SCREEN MODE");
+    touch = true;
+  }
   const grid = document.querySelector("#grid");
   for (let i = 0; i < 100; i++) {
     for (let j = 0; j < 100; j++) {
       const cell = document.createElement("div");
       cell.className = "cell";
       cell.id = `cell-${i}-${j}`;
-      cell.onmouseenter = () => {
-        cell.classList.add("animate");
-        const x = neighbours(i, j);
-        for (let p = 0; p < 4; p++) {
-          document
-            .querySelector(`#cell-${x[p][0]}-${x[p][1]}`)
-            ?.classList.add("animate");
-        }
-        setTimeout(() => {
-          cell.classList.remove("animate");
+      if (!touch) {
+        cell.onmouseenter = () => {
+          cell.classList.add("animate");
           const x = neighbours(i, j);
           for (let p = 0; p < 4; p++) {
             document
               .querySelector(`#cell-${x[p][0]}-${x[p][1]}`)
-              ?.classList.remove("animate");
+              ?.classList.add("animate");
           }
-        }, 1000);
-      };
+          setTimeout(() => {
+            cell.classList.remove("animate");
+            const x = neighbours(i, j);
+            for (let p = 0; p < 4; p++) {
+              document
+                .querySelector(`#cell-${x[p][0]}-${x[p][1]}`)
+                ?.classList.remove("animate");
+            }
+          }, FADEOUT_TIME);
+        };
+      } else {
+        cell.addEventListener("touchstart", (e) => {
+          touchFunc(e);
+        });
+        cell.addEventListener("touchend", (e) => {
+          touchFunc(e);
+        });
+        cell.addEventListener("touchcancel", (e) => {
+          touchFunc(e);
+        });
+        cell.addEventListener("touchmove", (e) => {
+          touchFunc(e);
+        });
+      }
       grid.appendChild(cell);
     }
   }
@@ -38,10 +58,55 @@ function neighbours(i, j) {
   ];
 }
 
-function makeArray(a, b) {
-  let arr = new Array(a);
-  for (let i = 0; i < a; i++) {
-    arr[i] = new Array(b);
+function random() {
+  return Math.floor(Math.random() * random_limit());
+}
+
+function random_limit() {
+  return Math.floor(Math.random() * 50);
+}
+function touchFunc(e) {
+  const id = e.target.id;
+  const [, x, y] = id.split("-");
+  document.querySelector(`#${id}`)?.classList.add("animate");
+  const elements = [[x, y]];
+  for (let count = 0; count < 10; count++) {
+    elements.push([parseInt(x) + random(), parseInt(y) + random()]);
   }
-  return arr;
+  for (let count = 0; count < 10; count++) {
+    elements.push([parseInt(x) - random(), parseInt(y) - random()]);
+  }
+  for (let count = 0; count < 10; count++) {
+    elements.push([parseInt(x) + random(), parseInt(y) - random()]);
+  }
+  for (let count = 0; count < 10; count++) {
+    elements.push([parseInt(x) - random(), parseInt(y) + random()]);
+  }
+  console.log(elements);
+  for (let p = 0; p < elements.length; p++) {
+    setTimeout(() => {
+      document
+        .querySelector(`#cell-${elements[p][0]}-${elements[p][1]}`)
+        .classList.add("animate");
+    }, p * 10);
+  }
+  setTimeout(() => {
+    for (let p = 0; p < elements.length; p++) {
+      setTimeout(() => {
+        document
+          .querySelector(`#cell-${elements[p][0]}-${elements[p][1]}`)
+          .classList.remove("animate");
+      }, p * 100);
+    }
+  }, FADEOUT_TIME);
+  // setTimeout(() => {
+  //   document.querySelector(`#${id}`)?.classList.remove("animate");
+  //   for (let i = x; i >= 0; i--) {
+  //     setTimeout(() => {
+  //       document
+  //         .querySelector(`#cell-${i}-${j}`)
+  //         ?.classList.remove("animate");
+  //     }, 10 * i);
+  //   }
+  // }, FADEOUT_TIME);
 }
